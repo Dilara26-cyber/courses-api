1981cc23d6308bd2c4bb9d97e2064c64fc375629
Dilara Aksoy <dilara_aksoy_00@hotmail.com>
2021-08-14 23:42:52 +0300

    Create Mavbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import logo from "../images/logo.png";
+import searchIcon from "../images/search.png";
+const Navbar = () => {
+  return (
+    <nav>
+      <section className="nav-section">
+        <img src={logo} alt="Logo"  className="logo"/>
+        <div className="searchBox">
+          <label htmlFor="searchInput">Search</label>
+          <input type="text" className="searchInput" id="searchInput" />
+          <img src={searchIcon} alt="Search Icon" />
+        </div>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
